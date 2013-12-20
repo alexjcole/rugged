31bea6ac24f79f23e8187fcf86685baada9c5956
Alex Cole <alexjcole@gmail.com>
2013-12-19 19:11:38 -0700

    Updated Ordering of pins via votes

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,8 +3,9 @@ class PinsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   def index
-   @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 4)
-  
+   #@pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 4)
+    @pins = Pin.all.sort_by{|pin|-pin.votes.size}
+
  end
 
   def show
