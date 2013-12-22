423785a26097717b4c4ed6276af483fc63e1fb54
eanplatter <ean@eanplatter.com>
2013-12-21 20:06:29 -0700

    mailr

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,17 @@
 class UserMailer < ActionMailer::Base
   # set some sensible defaults
-  default :from => Rugged::Application.config.postmark_signature
+  default :from => MyApp::Application.config.postmark_signature
+
+  # send a receipt of the Member's purchase
+  def receipt_message(order)
+    @order = order
+    @billing_address = order.convert_billing_address_to_hash(order.billing_address)
+
+    mail(:to => @order.user.email, :subject => "Your Order Receipt", :tag => 'order-receipt', :content_type => "text/html") do |format|
+      format.html
+    end
+  end
+
   # send password reset instructions
   def reset_password_instructions(user)
      @resource = user
