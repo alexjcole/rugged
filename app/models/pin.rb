1fb7dbe905d20836595f836de64b01d5366df2d9
eanplatter <ean.platter@gmail.com>
2013-12-28 19:21:22 +0000

    make descriptions optional

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,10 +2,9 @@ class Pin < ActiveRecord::Base
      belongs_to :user
      has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "100x100>" }
 
-validates :description, presence: true
 validates :image, presence: true
 acts_as_commontable
- acts_as_votable
+acts_as_votable
 
  
 
