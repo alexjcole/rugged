7cd1fe0447a5cda9a46debe82138a0c53a229ef1
eanplatter <eanplatter@gmail.com>
2013-12-30 16:17:01 +0000

    fixing uniqueness validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :pins
-  validates_uniqueness_of :username
+  validates_uniqueness_of :name
   validates :name, presence: true
   acts_as_commontator
   acts_as_voter
