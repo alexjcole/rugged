ea9edc06fc9e86f13cf37538e1a3ef40cc72c4cd
eanplatter <eanplatter@gmail.com>
2013-12-30 16:13:21 +0000

    usernames uniqueness validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :pins
+  validates_uniqueness_of :username
   validates :name, presence: true
   acts_as_commontator
   acts_as_voter
