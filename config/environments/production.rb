d49cef8f0eb9003362e0dc974a05841fa2e2663a
eanplatter <ean@eanplatter.com>
2013-12-31 14:56:47 -0700

    proc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,6 @@ OMRails::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-config.assets.digest = true
-config.assets.compile = false
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
