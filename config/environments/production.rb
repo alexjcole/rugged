d56ddd05054a607721654e22b9b1c99d0fd69e66
eanplatter <eanplatter@gmail.com>
2013-12-31 21:19:59 +0000

    omgomgomg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,9 @@ OMRails::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+config.assets.digest = true
+config.assets.compile = false
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
