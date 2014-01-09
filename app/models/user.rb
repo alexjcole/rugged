8051720439a684f054a32c2af89b70b04a4c505f
Alex Cole <alexjcole@gmail.com>
2014-01-09 11:02:05 -0700

    testing password name uniquness

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   
   has_many :pins
   # This makes Usernames unique. They're are just called "names" in this app though
-  validates_uniqueness_of :name
+
   validates :name, presence: true
   acts_as_commontator
   acts_as_voter
